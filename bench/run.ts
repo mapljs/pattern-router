ceb5bb23e38f564a52b97ee51bef908e4d8e1a1a
aquapi <aquaplmc@gmail.com>
2026-07-22 15:57:38 +0700

    add json reporter

--- bench/run.ts
+++ bench/run.ts
@@ -2,12 +2,14 @@ import type { Runnable } from 'measure-loop/api/types';
 
 import reporter from 'measure-loop/reporter';
 import md_reporter from 'measure-loop/reporter/md';
+import json_reporter from 'measure-loop/reporter/json';
 import { env } from 'measure-loop';
 
 export default <T extends Runnable>(meta: ImportMeta, b: T): T => {
   if (meta.main) {
     const format = process.argv.at(2);
     if (format === 'md') b.run({ env, reporter: md_reporter }).then(console.log);
+    else if (format === 'json') b.run({ env, reporter: json_reporter }).then(console.log);
     else b.run({ env, reporter });
   }
   return b;
