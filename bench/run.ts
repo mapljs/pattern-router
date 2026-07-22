3653f7e13778e3ebb00425b349c91c3250502489
aquapi <aquaplmc@gmail.com>
2026-07-22 15:55:56 +0700

    add md format option

--- bench/run.ts
+++ bench/run.ts
@@ -1,9 +1,14 @@
 import type { Runnable } from 'measure-loop/api/types';
 
 import reporter from 'measure-loop/reporter';
+import md_reporter from 'measure-loop/reporter/md';
 import { env } from 'measure-loop';
 
 export default <T extends Runnable>(meta: ImportMeta, b: T): T => {
-  meta.main && b.run({ env, reporter });
+  if (meta.main) {
+    const format = process.argv.at(2);
+    if (format === 'md') b.run({ env, reporter: md_reporter }).then(console.log);
+    else b.run({ env, reporter });
+  }
   return b;
 };
