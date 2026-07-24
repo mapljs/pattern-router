2dbaee9d7c0be84a0a428d64dfc7d50f91c00f9d
aquapi <aquaplmc@gmail.com>
2026-07-25 03:04:10 +0700

    better benchmark script

--- bench/run.ts
+++ bench/run.ts
@@ -4,13 +4,25 @@ import reporter from 'measure-loop/reporter';
 import md_reporter from 'measure-loop/reporter/md';
 import json_reporter from 'measure-loop/reporter/json';
 import { env } from 'measure-loop';
+import { writeFileSync } from 'node:fs';
 
 export default <T extends Runnable>(meta: ImportMeta, b: T): T => {
   if (meta.main) {
-    const format = process.argv.at(2);
-    if (format === 'md') b.run({ env, reporter: md_reporter }).then(console.log);
-    else if (format === 'json') b.run({ env, reporter: json_reporter }).then(console.log);
-    else b.run({ env, reporter });
+    const file = process.argv.at(2);
+    if (file) {
+      if (file.endsWith('.md')) {
+        b.run({ env, reporter: md_reporter }).then((content) => {
+          console.log(content);
+          writeFileSync(file, content);
+        });
+      } else if (file.endsWith('.json')) {
+        b.run({ env, reporter: json_reporter }).then((content) => {
+          let str = JSON.stringify(content, null, 2);
+          console.log(JSON.stringify(content, null, 2));
+          writeFileSync(file, str);
+        });
+      } else throw new Error('unknown extension');
+    } else b.run({ env, reporter });
   }
   return b;
 };
