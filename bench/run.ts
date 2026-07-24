e80f7f6580408b4d2080f4617f6a719062cff919
aquapi <aquaplmc@gmail.com>
2026-07-25 03:06:35 +0700

    fix

--- bench/run.ts
+++ bench/run.ts
@@ -17,8 +17,8 @@ export default <T extends Runnable>(meta: ImportMeta, b: T): T => {
         });
       } else if (file.endsWith('.json')) {
         b.run({ env, reporter: json_reporter }).then((content) => {
-          let str = JSON.stringify(content, null, 2);
-          console.log(JSON.stringify(content, null, 2));
+          const str = JSON.stringify(content, null, 2);
+          console.log(str);
           writeFileSync(file, str);
         });
       } else throw new Error('unknown extension');
